ab487bf67850211d7d41da63bf1cc7c35ea98cc1
mag <magiordano77@gmail.com>
2019-05-09 14:22:53 -0600

    finished findnewstreamers. probably want to add an average to each user

--- index.js
+++ index.js
@@ -1,89 +1,65 @@
 const express = require('express')
 const app = express()
+const services = require('./services')
 const monk = require('monk')
-const fetch = require('node-fetch');
+//const fetch = require('node-fetch');
 const port = 3000
 const url = 'mongodb://mag:3VZsQPNVkZ8aIGSc@cluster0-shard-00-00-z1he2.mongodb.net:27017,cluster0-shard-00-01-z1he2.mongodb.net:27017,cluster0-shard-00-02-z1he2.mongodb.net:27017/twitch_users?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
 const db = monk(url);
 db.then(() => {
-    console.log('Connected correctly to server')
-  })
-  collection = db.get('collection');
-
-const getTwitchData = async () => {
-    let arr = await fetch('https://api.twitch.tv/helix/streams/?first=100', {
-        method: 'get',
-        headers: {
-          'Client-ID': '7zkh4ut355tznqbv75vc1dsflxiu0v'
-        },
-      })
-      .then(res => res.json())
-  
-    pagination = arr.pagination.cursor
-  
-    let arr2 = await fetch('https://api.twitch.tv/helix/streams/?first=100&after=' + pagination, {
-        method: 'get',
-        headers: {
-          'Client-ID': '7zkh4ut355tznqbv75vc1dsflxiu0v'
-        },
-      })
-      .then(res => res.json())
-      let combine = arr.data.concat(arr2.data)
-      //combine both arr.data
-      //console.log(combine)
-    return combine;
-
-    }
-
-const findNewStreamers = async (arr) =>{
-    let newArr= [];  
-    await collection.find({})
-    .then((docs) => {
-       for(let i =0;i<arr.length; i++){
-            
-
-       }
-    
-       return newArr
-}
-
-    )}
+  console.log('Connected correctly to server')
+})
+collection = db.get('collection');
 
 //
 //successful query for date -30 
 //
-app.get('/',async(req, res) => {
-    let dateRemove = new Date();
-    dateRemove.setDate(dateRemove.getDate()-1);
-    console.log(dateRemove)
-   await collection.find({'data.date':{$lte: dateRemove} })
-        .then((docs) => {
-            res.send(docs)
-          })
+app.get('/', async (req, res) => {
+  let dateRemove = new Date();
+  dateRemove.setDate(dateRemove.getDate() - 1);
+  console.log(dateRemove)
+  await collection.find({
+      'data.date': {
+        $lte: dateRemove
+      }
+    })
+    .then((docs) => {
+      res.send(docs)
+    })
 
 
 })
 
-app.get('/sendResults',async(req, res) => {
-    let testDate = new Date();
-        //fetch twitch
-        //compare
-        //return results
-        let newResults = await getTwitchData();
-
-        //find if any new streamers
-        let test = await findNewStreamers(newResults);
-      //  dateRemove.setDate(dateRemove.getDate()-1);
-        //console.log(test)
-           
-       // console.log(newResults)
-
-        //console.log(test);
-        res.send(test);
+app.get('/sendResults', async (req, res) => {
+  //fetch twitch
+  //compare
+  //return results
+  const testDate = new Date();
 
 
-        //REQ time 
-        //query results of 
- 
-          })
+  //first, collect first time occurence
+  //streamers on request
+  const newResults = await services.getTwitchData();
+   collection.find({},'user_id').then(async (mongo) =>{
+    let newStreams = await services.findNewStreamers(newResults,mongo);
+    res.send(newStreams);
+
+  })
+  //find if any new streamers
+
+})
+
+
+
+
+app.get('/analysis', async (req, res) => {
+  //next, find average viewers of streams and see 
+  //if there are any trends
+  collection.find({},then((docs) =>{
+
+    
+    
+  }))
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ services.js
@@ -0,0 +1,66 @@
+const fetch = require('node-fetch');
+
+const fetchNewUsers = async (urlString) =>{
+
+    let newStreams = await fetch(urlString, {
+        method: 'get',
+        headers: {
+          'Client-ID': '7zkh4ut355tznqbv75vc1dsflxiu0v'
+        },
+      }).then(res => res.json())
+      
+      return newStreams
+}
+
+module.exports={
+    
+     findNewStreamers: findNewUsers = async (twitch,mongo) =>{
+        let urlString ="https://api.twitch.tv/helix/streams?"
+        
+        newUserIds = twitch.map((ele => ele.user_id))
+        mongoUserIds = mongo.map((ele => ele.user_id))
+
+        const newUsers =  newUserIds.filter(user => !mongoUserIds.includes(user))
+
+        //generate api string
+        for(let i=0;i<newUsers.length;i++){
+            urlString += "user_id="+  newUsers[i] + "&"
+        }
+
+        return fetchNewUsers(urlString)
+
+        },
+      
+      getTwitchData: getTwitchData = async () => {
+        let arr = await fetch('https://api.twitch.tv/helix/streams/?first=100', {
+            method: 'get',
+            headers: {
+              'Client-ID': '7zkh4ut355tznqbv75vc1dsflxiu0v'
+            },
+          })
+          .then(res => res.json())
+      
+        pagination = arr.pagination.cursor
+      
+        let arr2 = await fetch('https://api.twitch.tv/helix/streams/?first=100&after=' + pagination, {
+            method: 'get',
+            headers: {
+              'Client-ID': '7zkh4ut355tznqbv75vc1dsflxiu0v'
+            },
+          })
+          .then(res => res.json())
+        let combine = arr.data.concat(arr2.data)
+        //combine both arr.data
+        //console.log(combine)
+        return combine;
+      
+      },
+
+
+
+
+    
+
+}
+
+
