3f74323e69dfbc8aa119193cb1cca34c7cb83a59
mag <magiordano77@gmail.com>
2019-05-15 12:25:02 -0600

    a

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
-require('dotenv').config();
 const services = require('./services')
 const monk = require('monk')
 var cors = require('cors')
 //const fetch = require('node-fetch');
 const port = process.env.PORT||5000 
-const db = monk(process.env.MONGO_URL);
+const db = monk('mongodb://mag:3VZsQPNVkZ8aIGSc@cluster0-shard-00-00-z1he2.mongodb.net:27017,cluster0-shard-00-01-z1he2.mongodb.net:27017,cluster0-shard-00-02-z1he2.mongodb.net:27017/twitch_users?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true');
 db.then(() => {
   console.log('Connected correctly to server')
 })
